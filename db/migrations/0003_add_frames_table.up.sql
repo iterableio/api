9d6d1f06eee95b03eab2eb014ba90f475a866d0e
Rostislav Semenov <rrsbrg@gmail.com>
2016-04-02 18:58:17 -0700

    slight modifications to schemas

--- db/migrations/0003_add_frames_table.up.sql
+++ db/migrations/0003_add_frames_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE frames (
   id          uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   content     text,
-  editor      text NOT NULL,
+  editor      text,
   frame_taken timestamp without time zone NOT NULL,
   file_id     uuid NOT NULL,
   inserted_at timestamp without time zone NOT NULL DEFAULT now()
