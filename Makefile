d62554b0e9d8325f270128a57a270280102c2aa9
Rostislav Semenov <rrsbrg@gmail.com>
2016-04-04 21:08:56 -0700

    run tests in test db

--- Makefile
+++ Makefile
@@ -4,4 +4,7 @@ CONFIG_DIR_KEY := ITERABLE_CONFIG_DIR
 root_dir := $(shell pwd)
 
 test:
-	db/migrate reset; export $(CONFIG_DIR_KEY)=$(root_dir)/config; go test -v -cover ./...
+	export $(ENV_KEY)=test; \
+	export $(CONFIG_DIR_KEY)=$(root_dir)/config; \
+	db/migrate reset; \
+	go test -v -cover ./...
--- config/config_test.go
+++ config/config_test.go
@@ -17,11 +17,12 @@ func TestLoad(t *testing.T) {
 	assert := assert.New(t)
 
 	conf := IterableConfig{}
-	err := load(&conf, "development.yaml", yaml.Unmarshal)
-
+	err := load(&conf, "test.yaml", yaml.Unmarshal)
 	assert.Nil(err, "There was no error")
-	assert.Equal(conf.Postgres.DBName, "iterable", "This should be the same as development.yaml")
-	assert.Equal(conf.Postgres.SSLMode, "disable", "This should be the same as test.yaml")
+
+	same := "This should be the same as test.yaml"
+	assert.Equal(conf.Postgres.DBName, "iterable_test", same)
+	assert.Equal(conf.Postgres.SSLMode, "disable", same)
 }
 
 func TestLoadFileMissing(t *testing.T) {
@@ -50,7 +51,7 @@ func TestGetConfigPathFromEnv(t *testing.T) {
 
 	path := getConfigPath()
 
-	assert.Equal(path, "/hello/development.yaml")
+	assert.Equal(path, "/hello/test.yaml")
 }
 
 func TestGetConfigPathDefault(t *testing.T) {
@@ -61,5 +62,5 @@ func TestGetConfigPathDefault(t *testing.T) {
 
 	path := getConfigPath()
 
-	assert.Equal(path, "config/development.yaml")
+	assert.Equal(path, "config/test.yaml")
 }
